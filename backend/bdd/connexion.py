faf55ab2f49250e3fdc364e3eca37792fb5e3cbb
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-04-28 21:07:24 +0200

    ✨ feat: dotenv

--- backend/bdd/connexion.py
+++ backend/bdd/connexion.py
@@ -1,4 +1,6 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
 
 class Bdd: 
     def __init__(self, bdd_nom, bdd_port, bdd_host, bdd_user, bdd_mdp):
@@ -32,12 +34,14 @@ class Bdd:
         self.cursor.close()
         print("[INFO]: Deconnexion de la bdd OK")
 
+load_dotenv()
 con = Bdd(
-    bdd_nom=".env",
-    bdd_port=5432,
-    bdd_host=".env",
-    bdd_user=".env",
-    bdd_mdp=".env"
+    bdd_nom=os.getenv("BDD_NOM"),
+    bdd_port=os.getenv("BDD_PORT"),
+    bdd_host=os.getenv("BDD_HOST"),
+    bdd_user=os.getenv("BDD_USER"),
+    bdd_mdp=os.getenv("BDD_MDP")
 )
 
+# Test
 con.con()
