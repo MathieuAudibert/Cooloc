acbc984e719bd18e18b162eb5e1ce21f6722eadb
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-05-03 19:43:12 +0200

    🌈 style: creation du header + globals.css

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <div ckassName="gauche">
+                <img src="../../public/img/logo.png" alt="Logo" className="logo-image" />
+                <h1 className="title">Cooloc</h1>
+            </div>
+            <hr id="ligneHeader"></hr>
+        </header>
+    )
+}
+
+export default Header
