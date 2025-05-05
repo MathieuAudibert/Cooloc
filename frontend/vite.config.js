27d720347f8fdbfdf4bf51faeb5972f26cde8410
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-05-05 21:05:43 +0200

    🐞 fix: rollup linux

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,12 @@ export default defineConfig({
 
   server: {
     host: '0.0.0.0', 
-    port: 8080,
+    port: 3000,
     strictPort: true
   },
   preview: {
     host: '0.0.0.0', 
-    port: 8080,
+    port: 3000,
     strictPort: true
   }
 })
