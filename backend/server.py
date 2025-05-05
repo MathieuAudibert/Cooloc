acd41dbef71fd6d43d7af8cc4a735244102034e9
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-05-05 22:44:42 +0200

    ✨ feat: server python PROPRE

--- backend/server.py
+++ backend/server.py
@@ -2,7 +2,7 @@ import os
 import sys
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-HOST = "localhost"
+HOST = "192.168.1.71"
 PORT = 8000
 
 class Serveur(BaseHTTPRequestHandler):
@@ -11,13 +11,24 @@ class Serveur(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"Hello, world!")
+        self.wfile.write(b"OK")
 
     def do_POST(self):
         if self.path == '/login':
             self.send_header('Content-type', 'application/json')
             
             self.end_headers()
+
+        elif self.path == '/register':
+            self.send_header('Content-type', 'application/json')
+            
+            self.end_headers()
+
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(b'{"erreur": "pas trouve"}')
 
 serveur = HTTPServer((HOST, PORT), Serveur)
 print(f'le serveur ecoute sur http://{HOST}:{PORT}')
