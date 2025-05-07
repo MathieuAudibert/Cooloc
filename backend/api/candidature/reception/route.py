9f4e770180a3ac10faaa3f40afae9a381c1e3313
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-05-07 19:52:15 +0200

    ✨ feat: voir les candidatures plus tard

---
+++ backend/api/candidature/reception/route.py
@@ -0,0 +1,33 @@
+import jwt
+import sys
+import os
+from pathlib import Path
+racine = Path(__file__).resolve().parents[2]
+sys.path.append(str(racine))
+from bdd.connexion import con
+
+jwt_secret = os.getenv("JWT")
+jwt_algo = 'HS256'
+
+def verifier_token(data, token):
+    token_decode = jwt.decode(token, jwt_secret, algorithms=[jwt_algo])
+    
+    if token_decode['mail'] != data['mail'] and token_decode['role'] != data['role']:
+        return {'status': 400, 'message': 'Token invalide'}
+
+    if token_decode['role'] != 'responsable' or 'proprietaire':
+        return {'status': 403, 'message': 'Role invalide'}
+    
+    return {'status': 200, 'message': 'Token valide'}
+
+def recup_infos(utilisateur_id):
+    requete = """SELECT mail, prenom, nom FROM Utilisateurs WHERE id = %s LIMIT 1"""
+    con.cursor.execute(requete, (utilisateur_id,))
+    utilisateur = con.cursor.fetchone()
+    return utilisateur
+
+"""
+def voir_candidatures(data, token):
+    con.connexion()
+
+    infos_utilisateur = recup_infos(data['id_utilisateur'])"""
