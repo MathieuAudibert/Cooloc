eff43ab6c621accf02ad86056af0de804166c243
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-05-08 16:30:29 +0200

    ✨ feat: modification du nom de la coloc

---
+++ backend/api/colocations/modifications.py
@@ -0,0 +1,64 @@
+import sys
+import os
+from pathlib import Path
+from datetime import datetime
+import jwt
+projet_root = Path(__file__).resolve().parents[2]
+sys.path.append(str(projet_root))
+from bdd.connexion import con
+
+jwt_secret = os.getenv("JWT")
+jwt_algo = 'HS256'
+
+def verifier_token(data, token):
+    token_decode = jwt.decode(token, jwt_secret, algorithms=[jwt_algo])
+    
+    if token_decode['mail'] != data['mail'] and token_decode['role'] != data['role']:
+        return {'status': 403, 'message': 'Token KO'}
+    
+    if token_decode['role'] not in ['proprietaire', 'responsable', 'admin']:
+        return {'status': 403, 'message': 'Role KO'}
+    
+    return {'status': 200, 'message': 'Token OK'}
+
+def verifier_csrf(data):
+    csrf = data['csrf']
+
+    if not csrf:
+        return {'status': 403, 'message': 'CSRF KO'}
+
+    return {'status': 200, 'message': 'CSRF OK'}
+
+def recup_id(data) :
+    mail = data['mail']
+    requete = """SELECT id FROM Utilisateurs WHERE mail = %s LIMIT 1"""
+    con.cursor.execute(requete, (mail,))
+    id_utilisateur = con.cursor.fetchone()
+    return id_utilisateur
+
+def modifier_coloc(data, token): 
+    con.connexion()
+
+    id_utilisateur = recup_id(data)
+    if not id_utilisateur:
+        return {'status': 404, 'message': 'Utilisateur KO'}
+    
+    token_verif = verifier_token(data, token)
+    if token_verif['status'] != 200:
+        return token_verif
+    
+    csrf_verif = verifier_csrf(data)
+    if csrf_verif['status'] != 200:
+        return csrf_verif
+
+    requete = """UPDATE Colocs SET nom = %s WHERE id = %s"""
+    param = (data['nom'], data['id_coloc'])
+    con.cursor.execute(requete, param)
+    
+    requete2 = """INSERT INTO Logs (date, action, id_utilisateur, id_coloc) VALUES (%s, %s, %s, %s)"""
+    params = (datetime.now(), 'modification nom coloc', id_utilisateur, data['id_coloc'])
+    con.cursor.execute(requete2, params)
+
+    con.commit()
+    con.cursor.close()
+    return {'status': 200, 'message': 'Coloc modifiée avec succès'}
