7875e8989fdc7396025f309cc158eae0dfdea3ef
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-05-17 23:18:10 +0200

    ✨ feat: CRUD candidatures

---
+++ backend/api/candidature/voir/route.py
@@ -0,0 +1,60 @@
+import jwt
+import sys
+import os
+from pathlib import Path
+racine = Path(__file__).resolve().parents[2]
+sys.path.append(str(racine))
+from bdd.connexion import con
+
+jwt_secret = os.getenv("JWT")
+jwt_algo = 'HS256'
+
+def verifier_token(data, token):
+    token_decode = jwt.decode(token, jwt_secret, algorithms=[jwt_algo])
+    
+    if token_decode['mail'] != data['mail'] and token_decode['role'] != data['role']:
+        return {'status': 400, 'message': 'Token KO'}
+
+    if token_decode['role'] != 'responsable' or 'proprietaire':
+        return {'status': 403, 'message': 'Role KO'}
+    
+    return {'status': 200, 'message': 'Token OK'}
+
+def verifier_csrf(data):
+    csrf = data['csrf']
+
+    if not csrf:
+        return {'status': 403, 'message': 'CSRF KO'}
+
+    return {'status': 200, 'message': 'CSRF OK'}
+
+def recup_id(data) :
+    mail = data['mail']
+    requete = """SELECT id FROM Utilisateurs WHERE mail = %s LIMIT 1"""
+    con.cursor.execute(requete, (mail,))
+    id_utilisateur = con.cursor.fetchone()
+    return id_utilisateur
+
+def voir_candidatures(utilisateur_id):
+    con.connexion()
+
+    id_utilisateur = recup_id(data)
+    if not id_utilisateur:
+        return {'status': 404, 'message': 'Utilisateur KO'}
+    
+    token_verif = verifier_token(data, token)
+    if token_verif['status'] != 200:
+        return token_verif
+    
+    csrf_verif = verifier_csrf(data)
+    if csrf_verif['status'] != 200:
+        return csrf_verif
+
+    requete = """SELECT * FROM Candidatures WHERE id_utilisateur = %s"""
+    param = (id_utilisateur,)
+    con.cursor.execute(requete, param)
+    candidatures = con.cursor.fetchall()
+
+    return {'status': 200, 'message': 'Candidatures OK', 'data': candidatures}
+
+
