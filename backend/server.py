39c7b62e9594d1795010ace83427b274e83b0b91
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-06-02 22:48:08 +0200

    ✨ feat: cloturer une tache

--- backend/server.py
+++ backend/server.py
@@ -23,6 +23,7 @@ from api.colocations.taches.suppression import supprimer_tache
 from api.colocations.taches.voir import voir_tache
 from api.colocations.taches.attribue import attribuer_tache
 from api.colocations.taches.modifications import modifier_tache
+from api.colocations.taches.cloturer import cloturer_tache
 
 # admin 
 from api.adm.logi.supprimer import supprimer_logs
@@ -253,6 +254,13 @@ class Serveur(BaseHTTPRequestHandler):
 
         elif self.path == '/coloc/taches/attribuer':
             res = attribuer_tache(data, data['token'])
+            self.send_response(res['status'])
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(res).encode('utf-8'))
+
+        elif self.path == '/coloc/taches/cloturer':
+            res = cloturer_tache(data, data['token'])
             self.send_response(res['status'])
             self.send_header('Content-type', 'application/json')
             self.end_headers()
